552fe2b7cf0caa6c8e8be82fae9b6b58c1fd4c18
Anastasia Kolmagorova <canastie@gmail.com>
2024-07-23 21:35:30 +0300

    Реализация слайдера блока Жюри

--- source/js/main.js
+++ source/js/main.js
@@ -4,4 +4,38 @@
 // import 'swiper/css';
 
 import Swiper from "swiper";
-import { Navigation, Pagination } from "swiper/modules";
+import { Navigation } from "swiper/modules";
+
+const swiper = new Swiper('.swiper', {
+
+  modules: [Navigation],
+
+  loop: true,
+  // resizeObserver: false,
+  // slidesPerView: auto,
+
+  navigation: {
+    nextEl: '.persons__slider-button-next',
+    prevEl: '.persons__slider-button-prev',
+  },
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+      // width: 560,
+
+    },
+    1366: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+      touchRatio: 0,
+    }
+  },
+
+});
+
+
